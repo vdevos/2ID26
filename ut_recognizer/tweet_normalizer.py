50a9681eff7bd1c0abab265a658666d998b2c724
ThomHurks <thomhurks@gmail.com>
2014-10-16 21:10:33 +0200

    Tweet normalizer outputs in original file format
    Tweet normalizer now outputs the normalized tweets in the original
    input file format

--- ut_recognizer/tweet_normalizer.py
+++ ut_recognizer/tweet_normalizer.py
@@ -6,8 +6,10 @@ from nltk.stem import snowball
 
 
 # Input file:
-tweetsfilename = "tweets.txt"
-output_normalizedtweets_filename = "normalized_tweets.txt"
+input_tweets_filename = "tweets.txt"
+output_filename = "normalized_tweets"
+output_extension = ".txt"
+tweet_text_column_index = 5
 
 # The Snowball stemmer is basically "Porter v2".
 # There is also the Lancaster stemmer, but it is quite aggressive.
@@ -15,15 +17,22 @@ stemmer = snowball.EnglishStemmer()
 tweets = []
 normalized_tweets = []
 
+old_tweets = []
 
 # First we read in all the tweets.
-if os.path.isfile(tweetsfilename):
-    with open(tweetsfilename) as tweetsfile:
+if os.path.isfile(input_tweets_filename):
+    with open(input_tweets_filename) as tweetsfile:
         lines = tweetsfile.readlines()
         for line in lines:
             linedata = line.strip().split('\t')
-            if len(linedata) >= 6:
-                tweets.append(linedata[5])
+            if len(linedata) >= tweet_text_column_index + 1:
+                old_tweets.append(linedata)
+                tweets.append(linedata[tweet_text_column_index])
+            else:
+                print("Invalid file format!")
+                exit()
+
+assert len(old_tweets) == len(tweets)
 
 # Tokenize the tweets with the NLTK default tokenizer.
 for tweet in tweets:
@@ -34,14 +43,36 @@ for tweet in tweets:
             stemmed_token = stemmer.stem(token.lower())
             if len(stemmed_token) > 1:
                 normalized_tweet.append(stemmed_token)
-    if len(normalized_tweet) > 0:
-        normalized_tweets.append(normalized_tweet)
+    normalized_tweets.append(normalized_tweet)
+
+assert len(old_tweets) == len(normalized_tweets)
 
 space = str(' ')
-# The 'w' means open for exclusive creation, failing if the file already exists.
-with open(output_normalizedtweets_filename, 'x') as outputfile:
-    for tweet in normalized_tweets:
-        line = str()
-        for token in tweet:
-            line += token + space
-        outputfile.write(line + '\n')
+tab = str('\t')
+written = False
+file_counter = 0
+while not written:
+    try:
+        # The 'w' means open for exclusive creation, failing if the file already exists.
+        with open(output_filename + str(file_counter) + output_extension, 'x') as outputfile:
+            line_index = 0
+            for tweet in normalized_tweets:
+                line = str()
+                old_tweet = old_tweets[line_index]
+                column_index = 0
+                for column in old_tweet:
+                    if column_index < tweet_text_column_index:
+                        line += column + tab
+                    column_index += 1
+                for token in tweet:
+                    line += token + space
+                column_index = 0
+                for column in old_tweet:
+                    if column_index > tweet_text_column_index:
+                        line += tab + column
+                    column_index += 1
+                outputfile.write(line + '\n')
+                line_index += 1
+            written = True
+    except FileExistsError:
+        file_counter += 1
