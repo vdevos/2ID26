6e3a7abcc6c334f2a20bd58b2aba8b8911200114
Vincent de Vos <vin.de.vos@gmail.com>
2014-10-21 11:29:18 +0200

    Created pipeline script

---
+++ pipeline.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+verb()
+{
+    read -p ""
+}
+
+echo "1. Normalize tweets"
+verb 
+
+echo "2. Filter Noisy Terms"
+verb
+
+echo "3. Build inverted Index"
+verb
+
+echo "3. Extract Unidentified Terms"
+verb
+
+echo "4. Build Ranking"
+verb
+
+echo "4. Determine Context"
+verb
+
+echo "5. Cluster Terms"
+verb
+
+echo "6. Get Ranking"
+verb
+
+echo "6. Get Context"
+verb
+
+echo "6. Get Clustered Terms"
+verb
